0e6e2778a38fb960d79314960d8ad02c9c6aa300
Gaildon <algaildon@gmail.com>
2018-12-04 22:12:21 -0500

    Redirect to profile page on sign in, sign out button added to profile page.

--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,8 @@ const app = express();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
- app.get('/hello', (request, response) => {
-  //response.send("Hello from Firebase!");
-  //response.sendFile('../public/hello.html', {root: {root: __dirname }})
-   response.status(200).send(`<!doctype html>
-    <html>
-<header><title>This is title</title></header>
-<body>
-Hello world
-</body>
-</html>`);
+ app.get('/profile', (request, res) => {
+	res.sendFile('/public/profile.html', { "root" : __dirname});
  });
 
-exports.hello = functions.https.onRequest(app);
+exports.profile = functions.https.onRequest(app);
