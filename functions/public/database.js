0cbfe25d93f25a860de86d34b2ace24cc4b8a65b
Gaildon <algaildon@gmail.com>
2018-12-05 11:39:40 -0500

    Basic profile page, ladder creation working, ability to join ladders, ladder information page.

---
+++ functions/public/database.js
@@ -0,0 +1,41 @@
+
+(function() {
+
+const config = {
+    apiKey: "AIzaSyDYlEPVTwj8y123NMtD8tsQdo0osCXaPPE",
+    authDomain: "climb-7de83.firebaseapp.com",
+    databaseURL: "https://climb-7de83.firebaseio.com",
+    storageBucket: "gs://climb-7de83.appspot.com"
+  };
+ 	
+ 	if (!firebase.apps.length) {
+    	firebase.initializeApp(config);
+	}	
+ 	//firebase.initializeApp(config);
+
+
+	const preObject = document.getElementById('object');
+
+	const dbRefObject = firebase.database().ref().child('object');
+
+
+	dbRefObject.on('value', snap => console.log(snap.val()));
+
+	var database = firebase.database();
+
+
+	var ref = database.ref('ladders');
+
+	/*ref.child("test2").set({
+		value: "val"
+	
+	});*/
+
+	var data = {
+		value: "Hello"
+	}
+
+	//ref.push(data);
+
+}());
+
